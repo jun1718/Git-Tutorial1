bcf5e3e0bdc55de379555a784d9412e846980982
jun1718 <gbeovhsqhtka@naver.com>
2021-09-07 19:59:21 +0900

    Add module [Div]

--- module.java
+++ module.java
@@ -2,6 +2,7 @@ public class module{
     static int i=0;
     public static void add(int a){
         i+=a;
+        System.out.println("이건develop일때바꾼거다.")
     }
     public static void main(String[] args){
          System.out.println("develop에서 변경함");
