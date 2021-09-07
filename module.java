960dea08fd865a35b90da31bbb8cd9e44eb04ac5
jun1718 <gbeovhsqhtka@naver.com>
2021-09-07 20:11:09 +0900

    Add module [text]

--- module.java
+++ module.java
@@ -2,9 +2,12 @@ public class module{
     static int i=0;
     public static void add(int a){
         i+=a;
+        System.out.println("develop에서 추가함1");
+        System.out.println("dmasterp에서 추가함1");
     }
     public static void main(String[] args){
          System.out.println("develop에서 변경함");
          System.out.println("develop복습에서 변경함");
+         
     }
 }
